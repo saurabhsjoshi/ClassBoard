9ec8e6a17a9e022ebb6fbbf742bc7d020706bab5
Saurabh Joshi <battlex2010@yahoo.com>
2015-10-20 22:11:26 -0400

    Added Magnet SDK

--- app/src/main/java/com/sau/classboard/MainApplication.java
+++ app/src/main/java/com/sau/classboard/MainApplication.java
@@ -2,6 +2,8 @@ package com.sau.classboard;
 
 import android.app.Application;
 
+import com.magnet.mmx.client.api.MMX;
+
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 /**
@@ -12,6 +14,8 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        MMX.init(this, R.raw.classboard);
 
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
                 .setFontAttrId(R.attr.fontPath)
--- app/src/main/java/com/sau/classboard/activity/ChatActivity.java
+++ app/src/main/java/com/sau/classboard/activity/ChatActivity.java
@@ -6,5 +6,5 @@ import android.support.v7.app.AppCompatActivity;
  * Created by saurabh on 2015-10-18.
  */
 public class ChatActivity extends AppCompatActivity {
-
+    
 }
